3ff16d8c69ca3e92ec9d52a44a33961be5c68400
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-20 22:37:29 +0300

    added sass

--- bin/gulp/styles.js
+++ bin/gulp/styles.js
@@ -1,16 +1,16 @@
 var gulp = require("gulp");
-var stylus = require("gulp-stylus");
 var sourcemaps = require("gulp-sourcemaps");
 var concat = require("gulp-concat");
 var autoprefixer = require("gulp-autoprefixer");
 var uglifycss = require("gulp-uglifycss");
+var sass = require('gulp-sass');
 
 module.exports = function (config){
     return function(){
         config.forEach(function(styleConfig) {
             return gulp.src(styleConfig.src)
             .pipe(sourcemaps.init())
-            .pipe(stylus())
+            .pipe(sass())
             .pipe(autoprefixer())
             .pipe(uglifycss({
                 "max-line-len": 80
