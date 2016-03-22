6cfb4055ecf82116f9e008903bc98377c7230d52
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-22 19:56:50 +0300

    added forum page

--- src/dashboard/forum/service.js
+++ src/dashboard/forum/service.js
@@ -4,4 +4,16 @@ export default class ForumService {
     constructor(api) {
         this.api = api;
     }
+
+    getSections(page, sectionAmount, topicAmount) {
+        return this.api.get('/forum/sections', {
+            params: {
+                page: page,
+                topic_number: topicAmount,
+                per_page: sectionAmount
+            }
+        }).then(res=> {
+            return res.data;
+        })
+    }
 }
