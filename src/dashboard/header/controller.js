f3662d325a7506ef11a53181e7d7465868fbb1e2
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-25 22:00:03 +0300

    added logout logic

--- src/dashboard/header/controller.js
+++ src/dashboard/header/controller.js
@@ -1,14 +1,19 @@
 'use strict';
 
 export default class HeaderCtrl {
-    constructor(login, session, $state) {
+    constructor(login, session, $state, auth) {
         this.loginService = login;
         this.user = session.user;
         this.session = session;
         this.state = $state;
+        this.auth = auth;
     }
 
     openLoginPopup() {
         this.loginService.open();
+    }
+
+    logout() {
+        this.auth.logout();
     }
 }
