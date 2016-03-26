893d4165ead6ad3bb8c433840b4791d439d861a3
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-26 19:12:13 +0300

    added news-page breadcrumbs

--- src/dashboard/news-page/controller.js
+++ src/dashboard/news-page/controller.js
@@ -4,5 +4,16 @@
 export default class NewspageCtrl {
     constructor(news) {
         this.news = news;
+        this.rubric = news.rubric;
+        this.rubricState = this.getRubricState(this.rubric.api_path);
+    }
+
+    getRubricState(stateApiName) {
+        switch (stateApiName) {
+            case "blog":
+                return "dashboard.blog";
+            case "news":
+                return "dashboard.general-news";
+        }
     }
 }
