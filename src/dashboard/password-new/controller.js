87dbf011a4aed78ebc8f6502ed2436669fcd3e6d
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-27 23:55:15 +0300

    added password recover page

--- src/dashboard/password-new/controller.js
+++ src/dashboard/password-new/controller.js
@@ -2,6 +2,30 @@
 
 
 export default class PasswordnewCtrl {
-    constructor() {
+    constructor(passwordnewService, token, $modal) {
+        this.service = passwordnewService;
+        this.token = token;
+        this.modal = $modal;
+    }
+
+    changePassword() {
+        if (this.password != this.confirmPassword) {
+            this.modal.open({
+                template: '<div class="modal-body">Введенные пароли не совпадают!</div>'
+            });
+            return;
+        }
+        this.service.changePassword(this.token, this.password).
+            then((res)=>{
+                this.password = "";
+                this.confirmPassword = "";
+                this.modal.open({
+                    template: '<div class="modal-body">Пароль успешно изменен!</div>'
+                });
+            }).catch((err) => {
+                this.modal.open({
+                    template: '<div class="modal-body">Произошла ошибка. Пароль не был изменен!</div>'
+                });
+            });
     }
 }
