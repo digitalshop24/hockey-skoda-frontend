db0870610b10aae232e067edca7de7bad864dbcd
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-27 15:13:57 +0300

    added rendering teams on forecast page

--- src/dashboard/forecast/playoff/service.js
+++ src/dashboard/forecast/playoff/service.js
@@ -4,4 +4,10 @@ export default class ForecastService {
     constructor(api) {
         this.api = api;
     }
+
+    getTable() {
+        return this.api.get('/predictions/play_off').then(res=> {
+            return res.data;
+        });
+    }
 }
