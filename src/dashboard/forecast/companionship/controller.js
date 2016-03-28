dca11752f3ca567558fd04b7aa54a24ee32af5db
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-28 09:50:09 +0300

    added user predictions on companionship page

--- src/dashboard/forecast/companionship/controller.js
+++ src/dashboard/forecast/companionship/controller.js
@@ -4,6 +4,18 @@
 export default class ForecasttwoCtrl {
     constructor(matches, companionshipService) {
         this.matches = matches.matches;
+        this.userPredictions = matches.predictions;
+        this.matches.forEach((match) => {
+            match.redScore = 0;
+            match.blueScore = 0;
+            const id = match.id;
+            const userPrediction = this.userPredictions.find(x => x.match.id == id);
+            if(userPrediction) {
+                match.redScore = userPrediction.redteam_score;
+                match.blueScore = userPrediction.blueteam_score;
+                match.isDisabled = true;
+            }
+        });
         this.service = companionshipService;
     }
 
