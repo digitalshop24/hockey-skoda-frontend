cf15372cecca570c30cd62d77de2f676fb03f0bf
Savushkin Zhenya <savik94mail@gmail.com>
2016-03-28 23:52:13 +0300

    fixed companionship page if not authenticated

--- src/dashboard/forecast/companionship/controller.js
+++ src/dashboard/forecast/companionship/controller.js
@@ -5,22 +5,26 @@ export default class ForecasttwoCtrl {
     constructor(matches, companionshipService) {
         this.matches = matches.matches;
         this.userPredictions = matches.predictions;
-        this.matches.forEach((match) => {
-            match.redScore = 0;
-            match.blueScore = 0;
-            const id = match.id;
-            const userPrediction = this.userPredictions.find(x => x.match.id == id);
-            if(userPrediction) {
-                match.redScore = userPrediction.redteam_score;
-                match.blueScore = userPrediction.blueteam_score;
-                match.isDisabled = true;
-            }
-        });
+        if (this.userPredictions) {
+            this.matches.forEach((match) => {
+                match.redScore = 0;
+                match.blueScore = 0;
+                const id = match.id;
+                const userPrediction = this.userPredictions.find(x => x.match.id == id);
+                if (userPrediction) {
+                    match.redScore = userPrediction.redteam_score;
+                    match.blueScore = userPrediction.blueteam_score;
+                    match.isDisabled = true;
+                }
+            });
+        }
         this.service = companionshipService;
     }
 
     forecast(match) {
-        match.isDisabled = true;
-        this.service.sendForecast(match.id, match.redScore, match.blueScore);
+        this.service.sendForecast(match.id, match.redScore, match.blueScore).
+            then((res) => {
+                match.isDisabled = true;
+            });
     }
 }
