2cefed7c40431bcc08f4c7cc897bfb4f5411c1be
Savushkin Zhenya <savik94mail@gmail.com>
2016-04-01 22:40:14 +0300

    added video page

--- src/dashboard/video/controller.js
+++ src/dashboard/video/controller.js
@@ -2,6 +2,43 @@
 
 
 export default class VideoCtrl {
-    constructor() {
+    constructor(videoInfo, page, $state, $scope) {
+        this.videos = videoInfo.posts;
+        this.currentVideo = this.videos[0];
+        this.videoAmount = videoInfo.posts_count;
+        this.state = $state;
+        this.currentPage = page;
+        this.showInfo = true;
+        this.scope = $scope;
+    }
+
+
+    playVideo() {
+        this.showInfo = false;
+        this.player.playVideo();
+
+
+        this.scope.$on('youtube.player.paused', ($event, player) => {
+            console.log("time " + this.player.getCurrentTime());
+            console.log("state " + this.player.getPlayerState());
+            this.showInfo = true;
+
+            const secondsDuration = moment.duration(this.player.getCurrentTime(), 's');
+            const wholeSeconds = secondsDuration.seconds() < 10 ? "0" + secondsDuration.seconds() : secondsDuration.seconds();
+            this.videoTime = secondsDuration.minutes() + ":" + wholeSeconds;
+        });
+
+        this.scope.$on('youtube.player.playing', ($event, player) => {
+            this.showInfo = false;
+        });
+
+
+    }
+
+    loadMore() {
+        this.state.go('dashboard.video', {
+            videos: this.videos,
+            page: ++this.currentPage
+        });
     }
 }
--- src/dashboard/video/service.js
+++ src/dashboard/video/service.js
@@ -4,4 +4,15 @@ export default class VideoService {
     constructor(api) {
         this.api = api;
     }
+
+    getVideos(page, perPage) {
+        return this.api.get('/posts/video',{
+            params: {
+                page: page,
+                per_page: perPage
+            }
+        }).then((res) => {
+            return res.data;
+        })
+    }
 }
