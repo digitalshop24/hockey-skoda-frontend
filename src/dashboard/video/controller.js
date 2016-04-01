922b5c97ec12ef2be9bf936a7c427a5b019f88dd
Savushkin Zhenya <savik94mail@gmail.com>
2016-04-01 23:46:22 +0300

    added video page

--- src/dashboard/video/controller.js
+++ src/dashboard/video/controller.js
@@ -4,35 +4,72 @@
 export default class VideoCtrl {
     constructor(videoInfo, page, $state, $scope) {
         this.videos = videoInfo.posts;
+        this.videos.forEach((video, index) => {
+            video.index = index;
+        });
         this.currentVideo = this.videos[0];
         this.videoAmount = videoInfo.posts_count;
         this.state = $state;
         this.currentPage = page;
-        this.showInfo = true;
+        this.showVideoInfo = true;
         this.scope = $scope;
+        this.shouldPlayNewVideo = true;
+        this.scope.$on('youtube.player.ready', ($event, player) => {
+            player.cuePlaylist(this.videos.map(video => video.video_code));
+        });
     }
 
 
     playVideo() {
-        this.showInfo = false;
-        this.player.playVideo();
+        this.showVideoInfo = false;
 
+        if(this.shouldPlayNewVideo) {
+            this.player.playVideoAt(this.currentVideo.index);
+        } else {
+            this.player.playVideo()
+        }
 
-        this.scope.$on('youtube.player.paused', ($event, player) => {
-            console.log("time " + this.player.getCurrentTime());
-            console.log("state " + this.player.getPlayerState());
-            this.showInfo = true;
-
+        this.scope.$on('youtube.player.paused', () => {
+            this.showVideoInfo = true;
+            this.shouldPlayNewVideo = false;
             const secondsDuration = moment.duration(this.player.getCurrentTime(), 's');
             const wholeSeconds = secondsDuration.seconds() < 10 ? "0" + secondsDuration.seconds() : secondsDuration.seconds();
             this.videoTime = secondsDuration.minutes() + ":" + wholeSeconds;
         });
 
-        this.scope.$on('youtube.player.playing', ($event, player) => {
-            this.showInfo = false;
+        this.scope.$on('youtube.player.playing', () => {
+            this.showVideoInfo = false;
         });
 
+    }
 
+    next() {
+        const resultIndex = (this.currentVideo.index < this.videos.length - 1) ? this.currentVideo.index + 1 : 0;
+        this.step(resultIndex);
+    }
+
+    previous() {
+        const resultIndex = (this.currentVideo.index > 0) ? this.currentVideo.index - 1 : this.videos.length - 1;
+        this.step(resultIndex);
+    }
+
+    step(resultIndex) {
+        this.currentVideo = this.videos[resultIndex];
+        this.videoTime = undefined;
+        if(this.showVideoInfo) {
+            this.shouldPlayNewVideo = true;
+        } else {
+            this.player.playVideoAt(resultIndex);
+        }
+    }
+
+    setNewMainVideo(video) {
+        this.showVideoInfo = true;
+        this.shouldPlayNewVideo = true;
+        this.currentVideo = video;
+        this.videoTime = undefined;
+        this.player.pauseVideo();
+        window.scrollTo(0, 0);
     }
 
     loadMore() {
