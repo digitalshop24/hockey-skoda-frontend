9c7419db60408c9ca356666f99d81afe848549a9
gitwhatislove <whatislove0505@gmail.com>
2016-04-05 01:26:44 +0300

    fix page prizes

--- src/dashboard/prize-new/controller.js
+++ src/dashboard/prize-new/controller.js
@@ -3,5 +3,36 @@
 
 export default class PrizenewCtrl {
     constructor() {
+    	 this.responsive = [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                asNavFor:'.main_slider_priz'
+                
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                asNavFor:'.main_slider_priz'
+                
+              }
+            },
+            
+            {
+              breakpoint: 530,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                asNavFor:'.main_slider_priz'
+
+              }
+            }
+            
+          ];
     }
 }
