2a935f2b3ad53fc8cef7ca070af3c8ef08d01ac7
Savushkin Zhenya <savik94mail@gmail.com>
2016-04-08 23:53:29 +0300

    added normal logic

--- src/dashboard/socials/controller.js
+++ src/dashboard/socials/controller.js
@@ -2,9 +2,34 @@
 
 
 export default class SocialCtrl {
-    constructor(posts) {
+    constructor(posts, moment) {
+        const days = [];
         this.events = posts;
+        this.events.forEach(post => {
+            post.iconClass = post.network;
+            post.repostText = post.reposts + ' ' + this.getRepostText(post.network);
+
+            const temp = moment(post.published_at).format('YYYY-MM-DD');
+            var day = days.find(day => day.id == temp);
+            if(day) {
+                day.events.push(post);
+            } else {
+                days.push({
+                    id: temp,
+                    events: [post]
+                });
+            }
+        });
+        this.days = days;
+
+        var a = "";
     }
 
+    getRepostText(network) {
+        if(network == 'twitter') {
+            return 'ретвитов';
+        }
+        return 'репостов';
+    }
 
 }
