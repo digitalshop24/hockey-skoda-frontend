2eab5ddeec17d47d3703132180f68b684eda3d2a
gitwhatislove <whatislove0505@gmail.com>
2016-04-12 12:08:51 +0300

    add directive

--- src/dashboard/infographics/index.js
+++ src/dashboard/infographics/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import angular from 'angular';
+import social from './social/index.js';
 
 export default angular.module('dashboard.infographics', [])
     .config(function ($stateProvider) {
