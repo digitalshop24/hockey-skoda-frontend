4920759f28c3a2ea5c641dcd52ebbc6df8ce7860
Savushkin Zhenya <savik94mail@gmail.com>
2016-04-13 11:23:15 +0300

    #182 profile test drive

--- src/dashboard/profile/testdrive/index.js
+++ src/dashboard/profile/testdrive/index.js
@@ -2,14 +2,21 @@
 
 import angular from 'angular';
 import MytestdriveCtrl from './controller.js';
+import MytestdriveService from './service.js';
 
 export default angular.module('dashboard.profile.mytestdrive', [])
+    .service('mytestdriveService',MytestdriveService)
     .config(function ($stateProvider) {
         $stateProvider
             .state('dashboard.profile.mytestdrive', {
                 template: require('./template.html'),
                 url: '/my-testdrive',
                 controller: MytestdriveCtrl,
-                controllerAs: 'ctrl'
+                controllerAs: 'ctrl',
+                resolve: {
+                    coupons: mytestdriveService => {
+                        return mytestdriveService.getCoupons();
+                    }
+                }
             });
     });
