cc0b98873c52420aad757f10008f7eb7bb2a8dba
Savushkin Zhenya <savik94mail@gmail.com>
2016-04-25 01:05:33 +0300

    #658

--- src/dashboard/profile/prizes/controller.js
+++ src/dashboard/profile/prizes/controller.js
@@ -5,4 +5,12 @@ export default class PrizesCtrl {
     constructor(prizes) {
         this.prizes = prizes;
     }
+
+    save(href){
+        var a  = document.createElement('a');
+        var url = href;
+        a.href = url;
+        a.download = 'myTantam.png';
+        a.click();
+    }
 }
